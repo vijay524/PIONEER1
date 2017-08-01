ba67247611acf5559e18008c67ba05810d638ea1
vijay524 <kolli.vijaychowdary@gmail.com>
2017-07-31 20:33:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * C# Program to Swap two Numbers
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, equally;
+            Console.Write("\nEnter the First Number : ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("\nEnter the Second Number : ");
+            n2 = int.Parse(Console.ReadLine());
+            equally = n1;
+            n1 = n2;
+            n2 = equally;
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : " + n1);
+            Console.Write("\nSecond Number : " + n2);
+            Console.Read();
+        }
+    }
+}
